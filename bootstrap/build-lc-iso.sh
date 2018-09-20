a865675bcb68addd6d3731bfefc2665ef35be9d0
Steve Ison <si202@cam.ac.uk>
2018-09-20 12:11:20 +0100

    moving to distro

--- bootstrap/build-lc-iso.sh
+++ bootstrap/build-lc-iso.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-mkdir /tmp/ubuntu
-mount -o loop,rw  /tmp/ubuntu-16.04.5-desktop-amd64.iso /tmp/ubuntu
-#mount -o loop,rw  /tmp/lubuntu-16.04.5-desktop-amd64.iso /tmp/ubuntu
+mkdir /tmp/distro
+mount -o loop,rw  /tmp/distro.iso /tmp/distro
 mkdir /tmp/working-iso
-cp -a /tmp/ubuntu/* /tmp/working-iso
+cp -a /tmp/distro/* /tmp/working-iso
 cp /tmp/preseed.cfg /tmp/working-iso/preseed/
 cp /tmp/isolinux.cfg /tmp/working-iso/isolinux/
 cp /tmp/txt.cfg /tmp/working-iso/isolinux/
