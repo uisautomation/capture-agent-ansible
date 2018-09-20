2af21895f14e4de0e21c1644a95192e3002425ee
Steve Ison <si202@cam.ac.uk>
2018-09-20 14:35:07 +0100

    adding iso dir for output

--- bootstrap/build-lc-iso.sh
+++ bootstrap/build-lc-iso.sh
@@ -6,5 +6,5 @@ cp -a /tmp/distro/* /tmp/working-iso
 cp /tmp/preseed.cfg /tmp/working-iso/preseed/
 cp /tmp/isolinux.cfg /tmp/working-iso/isolinux/
 cp /tmp/txt.cfg /tmp/working-iso/isolinux/
-mkisofs -r -V "Capture Agent Install" -cache-inodes -J -l -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -o lc.iso /tmp/working-iso/
-isohybrid lc.iso
+mkisofs -r -V "Capture Agent Install" -cache-inodes -J -l -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -o ./iso/lc.iso /tmp/working-iso/
+isohybrid iso/lc.iso
